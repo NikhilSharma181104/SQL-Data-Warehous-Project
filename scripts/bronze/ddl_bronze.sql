575c95c8c325acb52f03f8e15900732f74a9f7be
Nikhil Sharma <nikhilsharma181104@gmail.com>
2026-07-02 12:13:16 +0530

    Create Bronze schema and associated tables
    This script creates the Bronze schema and defines multiple tables for customer and product information, as well as sales details and ERP data.

---
+++ scripts/bronze/ddl_bronze.sql
@@ -0,0 +1,98 @@
+/*
+=============================================================
+Create Bronze Schema
+=============================================================
+*/
+
+IF NOT EXISTS (
+    SELECT 1
+    FROM sys.schemas
+    WHERE name = 'bronze'
+)
+BEGIN
+    EXEC('CREATE SCHEMA bronze');
+END;
+GO
+
+/*
+=============================================================
+Drop Existing Tables
+=============================================================
+*/
+
+DROP TABLE IF EXISTS bronze.crm_cust_info;
+DROP TABLE IF EXISTS bronze.crm_prd_info;
+DROP TABLE IF EXISTS bronze.crm_sales_details;
+DROP TABLE IF EXISTS bronze.erp_cust_az12;
+DROP TABLE IF EXISTS bronze.erp_loc_a101;
+DROP TABLE IF EXISTS bronze.erp_px_cat_g1v2;
+GO
+
+/*
+=============================================================
+Create CRM Tables
+=============================================================
+*/
+
+CREATE TABLE bronze.crm_cust_info
+(
+    cst_id INT,
+    cst_key NVARCHAR(50),
+    cst_firstname NVARCHAR(50),
+    cst_lastname NVARCHAR(50),
+    cst_marital_status NVARCHAR(50),
+    cst_gndr NVARCHAR(50),
+    cst_create_date DATE
+);
+
+CREATE TABLE bronze.crm_prd_info
+(
+    prd_id INT,
+    prd_key NVARCHAR(50),
+    prd_nm NVARCHAR(50),
+    prd_cost DECIMAL(18,2),
+    prd_line NVARCHAR(50),
+    prd_start_dt DATE,
+    prd_end_dt DATE
+);
+
+CREATE TABLE bronze.crm_sales_details
+(
+    sls_ord_num NVARCHAR(50),
+    sls_prd_key NVARCHAR(50),
+    sls_cust_id INT,
+    sls_order_dt INT,
+    sls_ship_dt INT,
+    sls_due_dt INT,
+    sls_sales INT,
+    sls_quantity INT,
+    sls_price INT
+);
+
+/*
+=============================================================
+Create ERP Tables
+=============================================================
+*/
+
+CREATE TABLE bronze.erp_cust_az12
+(
+    cid NVARCHAR(50),
+    bdate DATE,
+    gen NVARCHAR(50)
+);
+
+CREATE TABLE bronze.erp_loc_a101
+(
+    cid NVARCHAR(50),
+    cntry NVARCHAR(50)
+);
+
+CREATE TABLE bronze.erp_px_cat_g1v2
+(
+    id NVARCHAR(50),
+    cat NVARCHAR(50),
+    subcat NVARCHAR(50),
+    maintenance NVARCHAR(50)
+);
+GO
